cc844a2f7dbf6786f1f2db8eb36dbab7c9fa1afb
Roman Gonzalez <open-source@roman-gonzalez.info>
2019-05-23 02:31:22 +0000

    Add more code to the example

--- example/example.go
+++ example/example.go
@@ -8,24 +8,41 @@ import (
 var myWorker := w.Go(
 )
 
-var mySubSystem := s.NewSupervisor("UDP connection manager",
-....
-)
+var mySubSystem := s.NewSupervisor("UDP connection manager")
 
 func newMyWorker(index int) {
 	return w.Go(fmt.Sprintf("worker-%d", index))
 }
 
+func logSupervisorEventWorker(c chan ProcessEvent) w.Worker {
+	return w.Go("event-logger",
+		func(ctx context.Context) error {
+			for ev := range c {
+				select {
+					case <-ctx.Done():
+					  return ctx.Err()
+					default:
+					fmt.Printf("%v\n", ev)
+				}
+			}
+		})
+}
+
+type ProcessEvent struct {}
+
 func main() {
+	c := make(chan ProcessEvent, 100)
 	x := c.NewSupervisor(
 		"main",
+		s.WithObserverChan(c)
 		s.WithContext(context.Background()),
-		s.WithStrategy(c.AllForOne),
+		s.WithStrategy(s.AllForOne),
 		s.WithWorkerBootJitter(10 * time.Milliseconds)
 		s.WithTolerance(10, 10 * time.Second),
-		s.WithStartOrder(c.LeftToRight),
+		s.WithStartOrder(s.LeftToRight),
 		s.WithPanicRescue(true),
 		s.WithProcesses(
+			logSupervisorEventWorker(c),
 			w.Go("foo",
 				func(ctx context.Context) { },
 				w.WithBackoff(c.ExponentialBackoff),
@@ -33,12 +50,15 @@ func main() {
 				w.WithRunTimeout(1 * time.Minute),
 				w.WithWorkerStrategy(c.Permanent),
 				w.WithWorkerPanicRescue(true),
+				w.WithOnError(func(err error) {
+
+				})
 			),
 			s.NewSupervisor(
 				"child-1"
-				c.WithSupStrategy(c.AllForOne),
-				c.WithWorkerBootJitter(10 * time.Milliseconds)
-				c.WithTolerance(10, 10 * time.Second),
+				s.WithStrategy(c.AllForOne),
+				s.WithWorkerBootJitter(10 * time.Milliseconds)
+				s.WithTolerance(10, 10 * time.Second),
 			),
 			mySubSystem,
 			myWorker,
