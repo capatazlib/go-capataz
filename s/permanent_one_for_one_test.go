53a7b39bbff9b0f045930d861f4d5df5a62fff35
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-03-18 14:13:06 -0700

    Standarize on variable names

--- s/permanent_one_for_one_test.go
+++ s/permanent_one_for_one_test.go
@@ -19,7 +19,7 @@ import (
 func TestPermanentOneForOneSingleFailingChildRecovers(t *testing.T) {
 	parentName := "root"
 	// Fail only one time
-	child1, startFailingChild1 := FailOnSignalChild(1, "child1", c.WithRestart(c.Permanent))
+	child1, failChild1 := FailOnSignalChild(1, "child1", c.WithRestart(c.Permanent))
 
 	events, err := ObserveSupervisor(
 		context.TODO(),
@@ -34,7 +34,7 @@ func TestPermanentOneForOneSingleFailingChildRecovers(t *testing.T) {
 			// 1) Wait till all the tree is up
 			evIt.SkipTill(SupervisorStarted("root"))
 			// 2) Start the failing behavior of child1
-			startFailingChild1()
+			failChild1()
 			// 3) Wait till first restart
 			evIt.SkipTill(WorkerStarted("root/child1"))
 		},
@@ -47,7 +47,7 @@ func TestPermanentOneForOneSingleFailingChildRecovers(t *testing.T) {
 			// start children from left to right
 			WorkerStarted("root/child1"),
 			SupervisorStarted("root"),
-			// ^^^ 1) startFailingChild1 starts executing here
+			// ^^^ 1) failChild1 starts executing here
 			WorkerFailed("root/child1"),
 			// ^^^ 2) And then we see a new (re)start of it
 			WorkerStarted("root/child1"),
@@ -61,7 +61,7 @@ func TestPermanentOneForOneSingleFailingChildRecovers(t *testing.T) {
 func TestPermanentOneForOneNestedFailingChildRecovers(t *testing.T) {
 	parentName := "root"
 	// Fail only one time
-	child1, startFailingChild1 := FailOnSignalChild(1, "child1", c.WithRestart(c.Permanent))
+	child1, failChild1 := FailOnSignalChild(1, "child1", c.WithRestart(c.Permanent))
 	tree1 := s.New("subtree1", s.WithChildren(child1))
 
 	events, err := ObserveSupervisor(
@@ -75,7 +75,7 @@ func TestPermanentOneForOneNestedFailingChildRecovers(t *testing.T) {
 			// 1) Wait till all the tree is up
 			evIt.SkipTill(SupervisorStarted("root"))
 			// 2) Start the failing behavior of child1
-			startFailingChild1()
+			failChild1()
 			// 3) Wait till first restart
 			evIt.SkipTill(WorkerStarted("root/subtree1/child1"))
 		},
@@ -91,7 +91,7 @@ func TestPermanentOneForOneNestedFailingChildRecovers(t *testing.T) {
 			SupervisorStarted("root"),
 			// ^^^ 1) Wait till root starts
 			WorkerFailed("root/subtree1/child1"),
-			// ^^^ 2) We see the startFailingChild1 causing the error
+			// ^^^ 2) We see the failChild1 causing the error
 			WorkerStarted("root/subtree1/child1"),
 			// ^^^ 3) After 1st (re)start we stop
 			WorkerStopped("root/subtree1/child1"),
