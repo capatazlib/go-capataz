6925d4ae870d5513d8d0540b28c6d838c95f154f
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-04-05 19:21:51 -0700

    Remove example from public_test given it was not realistic

--- s/public_test.go
+++ s/public_test.go
@@ -7,7 +7,6 @@ package s_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,97 +15,6 @@ import (
 	. "github.com/capatazlib/go-capataz/internal/stest"
 	"github.com/capatazlib/go-capataz/s"
 )
-
-// ExampleNew showcases an example on how to define a supervision tree system.
-//
-// In this trivial example we create a system with two sub-systems.
-//
-// 1) The "file-system" sub-tree supervises file system errors
-//
-// 2) The "service-a" sub-tree supervises interactions with the service-a API
-//
-// Note that if there are errors in the "file-system" sub-tree, its supervisor
-// will deal with transient errors without affecting the "service-a" sub-tree.
-//
-// If errors on the file-system go beyond an specified treshold, the errors are
-// raised to the root supervisor and the root supervisor will restart both
-// sub-trees.
-//
-// If the errors still continue on the "file-system" sub-tree, it will
-// eventually surpass the root tree error treshold and the system will return an
-// error on the root supervisor Wait call.
-//
-func ExampleNew() {
-	// // Channels used for communication between the different children of the
-	// // supervision tree
-	// fileChangedCh := make(chan string)
-	writeFileCh := make(chan string)
-
-	// Build a supervision tree with two branches
-	rootSpec := s.New(
-		"root",
-		// first sub-tree
-		s.WithSubtree(
-			s.New("file-system",
-				s.WithChildren(
-					c.New("file-watcher", func(ctx context.Context) error {
-						fmt.Println("Start File Watch Functionality")
-						// Use inotify and send messages to the fileChangedCh
-						<-ctx.Done()
-						return nil
-					}),
-					c.New("file-writer-manager", func(ctx context.Context) error {
-						// assume this function has access to a request chan from a closure
-						for {
-							select {
-							case <-ctx.Done():
-								return ctx.Err()
-							case _ /* content */, ok := <-writeFileCh:
-								if !ok {
-									err := fmt.Errorf("writeFileCh ownership compromised")
-									return err
-								}
-								fmt.Println("Write contents to a file")
-							}
-						}
-					}),
-				),
-			),
-		),
-		// second sub-tree
-		s.WithSubtree(
-			s.New("service-a",
-				s.WithChildren(
-					c.New("fetch-service-data-ticker", func(ctx context.Context) error {
-						// assume this function has access to a request and response
-						// channnels from a closure
-						fmt.Println("Perform requests to API to gather data and submit it to a channel")
-						<-ctx.Done()
-						return nil
-					}),
-				),
-			),
-		),
-	)
-
-	// Spawn goroutines of supervision tree
-	sup, err := rootSpec.Start(context.Background())
-	if err != nil {
-		fmt.Printf("Error starting system: %v\n", err)
-		return
-	}
-
-	// Wait for supervision tree to exit, this will only happen when errors cannot
-	// be recovered by the supervision system because they reached the given
-	// treshold of failure.
-	//
-	// TODO: Add reference to treshold settings documentation once it is
-	// implemented
-	err = sup.Wait()
-	if err != nil {
-		fmt.Printf("Supervisor failed with error: %v\n", err)
-	}
-}
 
 func TestStartSingleChild(t *testing.T) {
 	events, err := ObserveSupervisor(
