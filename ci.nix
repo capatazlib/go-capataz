ec9a22e79d3c80c7c0ad67bd1df79c49238ee704
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-05-20 18:31:54 -0700

    Add an specific ci.nix file

---
+++ ci.nix
@@ -0,0 +1,35 @@
+let
+  sources = import ./nix/sources.nix {};
+
+  # build an overlay with specific packages from sources.nix
+  overlay = _: pkgs: {
+    niv = import sources.niv {};
+  };
+
+  # get nixpkgs with pinned packages overlay
+  pinnedPkgs = import sources.nixpkgs {
+    overlays = [overlay];
+  };
+in
+
+# This allows overriding pkgs by passing `--arg pkgs ...`
+{ pkgs ? pinnedPkgs }:
+
+let
+  go-capataz = import ./default.nix { pkgs = pkgs; };
+
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      # current go version
+      go_1_14
+      go-capataz
+
+      # linting
+      gotools godef golint golangci-lint
+    ];
+
+    shellHook = ''
+      unset GOPATH
+    '';
+  }
