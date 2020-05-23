5183d8f5cc33b513df6f676f13ea2e3578f72582
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-05-23 15:42:47 -0700

    Remove pinning in the lib derivation, and make deps explicit

--- default.nix
+++ default.nix
@@ -1,31 +1,11 @@
-let
-  sources = import ./nix/sources.nix {};
-  pinnedPkgs = import sources.nixpkgs {};
+{ buildGoPackage, go, lib }:
 
-in
+assert lib.versionAtLeast go.version "1.14";
 
-{ pkgs ? pinnedPkgs }:
-
-assert pkgs.lib.versionAtLeast pkgs.go.version "1.14";
-
-pkgs.buildGoPackage rec {
+buildGoPackage rec {
   name = "go-capataz";
   version = "latest";
   goPackagePath = "github.com/capatazlib/go-capataz";
   src = ./.;
   goDeps = ./deps.nix;
 }
-
-# pkgs.buildGoModule rec {
-#   name = "go-capataz";
-#   version = "latest";
-#   src = ./.;
-#   vendorSha256 = "0zv0nfbc1qkj5jq9ax3da7g7jr1fqbb7zhy8sqis1h9jgzl06x2s";
-#   meta = with pkgs.lib; {
-#     description = "";
-#     homepage = https://github.com/capatazlib/go-capataz;
-#     license = licenses.mit;
-#     maintainers = [ "Roman Gonzalez" ];
-#     platforms = platforms.linux ++ platforms.darwin;
-#   };
-# }
