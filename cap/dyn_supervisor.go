2285a104069147f3de393912acfe52a04eb71f51
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-10-10 04:33:25 -0700

    refactor control message implementation to a more OO style #review

--- cap/dyn_supervisor.go
+++ cap/dyn_supervisor.go
@@ -9,13 +9,87 @@ import (
 	"github.com/capatazlib/go-capataz/internal/c"
 )
 
+// ctrlMsg executes some control logic on the Supervisor goroutine
+type ctrlMsg interface {
+	// processMsg receives all the required supervisor state to fullfill
+	// its purpose
+	processMsg(
+		evNotifier EventNotifier,
+		spec SupervisorSpec,
+		specChildren []c.ChildSpec,
+		supRuntimeName string,
+		supChildren map[string]c.Child,
+		supNotifyCh chan c.ChildNotification,
+	) ([]c.ChildSpec, map[string]c.Child)
+}
+
+type startChildResult struct {
+	childName string
+	startErr  startError
+}
+
 // startChildMsg is a message sent from clients to tell a supervisor to spawn
 // on-demand a worker routine.
 type startChildMsg struct {
 	node Node
 	// result could either be a startError or a string with the runtime name
-	resultChan chan<- interface{}
+	resultChan chan<- startChildResult
 }
+
+func (scm startChildMsg) processMsg(
+	evNotifier EventNotifier,
+	spec SupervisorSpec,
+	specChildren []c.ChildSpec,
+	supRuntimeName string,
+	supChildren map[string]c.Child,
+	supNotifyCh chan c.ChildNotification,
+) ([]c.ChildSpec, map[string]c.Child) {
+	// REMEMBER: WE ARE RUNNING THIS CODE IN THE SUPERVISOR THREAD
+
+	childSpec := scm.node(spec)
+
+	ch, startErr := startChildNode(spec, supRuntimeName, supNotifyCh, childSpec)
+	if startErr != nil {
+		// When we fail, we send an error to the supNotifyCh and return the error,
+		// this doesn't have any detrimental consequence in static supervisors,
+		// but on dynamic supervisors, it means the monitor loop will get bothered
+		// with an error that it should not really handle. We are going to read it
+		// out and return after that.
+		_ = <-supNotifyCh
+		// do not block waiting for a read
+		select {
+		case scm.resultChan <- startChildResult{
+			childName: "",
+			startErr:  startErr,
+		}:
+		default:
+		}
+
+		return specChildren, supChildren
+
+	}
+
+	// We store the child to the spec list because we need to terminate them
+	// when the supervisor is terminated in the correct order. This won't have
+	// unintended side-effects because a DynSupervisor once terminated, cannot
+	// be started again.
+	specChildren = append(specChildren, childSpec)
+	supChildren[ch.GetName()] = ch
+
+	select {
+	case scm.resultChan <- startChildResult{
+		childName: ch.GetName(),
+		startErr:  nil,
+	}:
+	// We return the regular name (not the runtime one), given that we store that
+	// in the childrenMap of the supervisor
+	default:
+	}
+
+	return specChildren, supChildren
+}
+
+var _ ctrlMsg = startChildMsg{}
 
 // terminateChildMsg is a message sent from clients to tell a supervisor to close a
 // previously spawned worker routine.
@@ -24,20 +98,51 @@ type terminateChildMsg struct {
 	resultChan chan<- terminateError
 }
 
-// ctlrMsgTag tells us which control message we need to cast/handle
-type ctrlMsgTag uint32
+func (tcm terminateChildMsg) processMsg(
+	evNotifier EventNotifier,
+	spec SupervisorSpec,
+	specChildren []c.ChildSpec,
+	supRuntimeName string,
+	supChildren map[string]c.Child,
+	supNotifyCh chan c.ChildNotification,
+) ([]c.ChildSpec, map[string]c.Child) {
+	// REMEMBER: WE ARE RUNNING THIS CODE IN THE SUPERVISOR THREAD
 
-const (
-	startChild ctrlMsgTag = iota
-	terminateChild
-)
+	ch, ok := supChildren[tcm.nodeName]
+	if !ok {
+		errMsg := fmt.Sprintf("worker %s not found", tcm.nodeName)
+		// do not block waiting for a read
+		select {
+		case tcm.resultChan <- errors.New(errMsg):
+		default:
+		}
 
-// ctlrMsg is a message sent from clients to a supervisor via a public API
-// method
-type ctrlMsg struct {
-	tag ctrlMsgTag
-	msg interface{}
+		return specChildren, supChildren
+	}
+
+	// we call our basic terminateChildNode function that is found in the
+	// monitor.go file
+	terminateErr := terminateChildNode(evNotifier, ch)
+
+	// do not block waiting for a read
+	select {
+	case tcm.resultChan <- terminateErr:
+	default:
+	}
+
+	// we remove the terminated child from the spec and the runtime children to
+	// avoid shutting it down on supervisor termination
+	for i, chSpec := range specChildren {
+		if chSpec.GetName() == ch.GetName() {
+			specChildren = append(specChildren[:i], specChildren[i+1:]...)
+			delete(supChildren, ch.GetName())
+		}
+	}
+
+	return specChildren, supChildren
 }
+
+var _ ctrlMsg = terminateChildMsg{}
 
 // DynSupervisor is a supervisor that can spawn workers in a procedural way.
 type DynSupervisor struct {
@@ -50,99 +155,30 @@ type DynSupervisor struct {
 // API calls like Spawn or Cancel a child node.
 func handleCtrlMsg(
 	eventNotifier EventNotifier,
-	supSpec SupervisorSpec,
-	supChildrenSpec []c.ChildSpec,
+	spec SupervisorSpec,
+	specChildren []c.ChildSpec,
 	supRuntimeName string,
 	supChildren map[string]c.Child,
 	supNotifyCh chan c.ChildNotification,
 	msg ctrlMsg,
 ) ([]c.ChildSpec, map[string]c.Child) {
-	switch msg.tag {
-	case startChild:
-		startcm, ok := msg.msg.(startChildMsg)
-		if !ok {
-			panic("Expected startChildMsg; got something else. Invalid Supervisor implementation")
-		}
-		childSpec := startcm.node(supSpec)
-
-		ch, startErr := startChildNode(supSpec, supRuntimeName, supNotifyCh, childSpec)
-		if startErr != nil {
-			// When we fail, we send an error to the supNotifyCh and return the error,
-			// this doesn't have any detrimental consequence in static supervisors,
-			// but on dynamic supervisors, it means the monitor loop will get bothered
-			// with an error that it should not really handle. We are going to read it
-			// out and return after that.
-			_ = <-supNotifyCh
-			// do not block waiting for a read
-			select {
-			case startcm.resultChan <- startErr:
-			default:
-			}
-
-			return supChildrenSpec, supChildren
-		}
-
-		// We store the child to the spec list because we need to terminate them
-		// when the supervisor is terminated in the correct order. This won't have
-		// unintended side-effects because a DynSupervisor once terminated, cannot
-		// be started again.
-		supChildrenSpec = append(supChildrenSpec, childSpec)
-		supChildren[ch.GetName()] = ch
-
-		select {
-		case startcm.resultChan <- ch.GetName():
-			// We return the regular name (not the runtime one), given that we store that
-			// in the childrenMap of the supervisor
-		default:
-		}
-
-		return supChildrenSpec, supChildren
-
-	case terminateChild:
-		terminatecm, ok := msg.msg.(terminateChildMsg)
-		ch, ok := supChildren[terminatecm.nodeName]
-		if !ok {
-			errMsg := fmt.Sprintf("worker %s not found", terminatecm.nodeName)
-			// do not block waiting for a read
-			select {
-			case terminatecm.resultChan <- errors.New(errMsg):
-			default:
-			}
-
-			return supChildrenSpec, supChildren
-		}
-
-		// we call our basic terminateChildNode function that is found in the
-		// monitor.go file
-		terminateErr := terminateChildNode(eventNotifier, ch)
-		// do not block waiting for a read
-		select {
-		case terminatecm.resultChan <- terminateErr:
-		default:
-		}
-
-		// we remove the terminated child from the spec and the runtime children to
-		// avoid shutting it down on supervisor termination
-		for i, chSpec := range supChildrenSpec {
-			if chSpec.GetName() == ch.GetName() {
-				supChildrenSpec = append(supChildrenSpec[:i], supChildrenSpec[i+1:]...)
-				delete(supChildren, ch.GetName())
-			}
-		}
-
-		return supChildrenSpec, supChildren
-
-	default:
-		panic("Unknown control message received. Invalid Supervisor implementation.")
-	}
+	return msg.processMsg(
+		eventNotifier,
+		spec,
+		specChildren,
+		supRuntimeName,
+		supChildren,
+		supNotifyCh,
+	)
 }
 
 func (dyn *DynSupervisor) terminateNode(nodeName string) func() error {
+	// REMEMBER: WE ARE RUNNING ON THE CLIENT API THREAD
 
 	// we initialize the resultCh with a buffer of 1, we may store the result
 	// before the client is ready to read it.
 	resultCh := make(chan terminateError, 1)
-	terminatemsg := terminateChildMsg{
+	msg := terminateChildMsg{
 		nodeName:   nodeName,
 		resultChan: resultCh,
 	}
@@ -163,10 +199,6 @@ func (dyn *DynSupervisor) terminateNode(nodeName string) func() error {
 		}()
 		// block until the supervisor can handle the request, in case the
 		// supervisor is stopped, this line is going to panic
-		msg := ctrlMsg{
-			tag: terminateChild,
-			msg: terminatemsg,
-		}
 		select {
 		case dyn.sup.ctrlCh <- msg:
 		case _ = <-time.After(1 * time.Second):
@@ -192,6 +224,8 @@ func (dyn *DynSupervisor) terminateNode(nodeName string) func() error {
 // start of this worker failed. This function blocks until the worker is
 // started.
 func (dyn *DynSupervisor) Spawn(nodeFn Node) (func() error, error) {
+	// REMEMBER: WE ARE RUNNING ON THE CLIENT API THREAD
+
 	// if we already registered a terminationErr, return it
 	if dyn.terminated {
 		return nil, fmt.Errorf("supervisor already terminated: %w", dyn.terminationErr)
@@ -206,17 +240,10 @@ func (dyn *DynSupervisor) Spawn(nodeFn Node) (func() error, error) {
 
 	// we initialize the resultCh with a buffer of 1, we may store the result
 	// before the client is ready to read it.
-	resultCh := make(chan interface{}, 1)
-	startmsg := startChildMsg{
+	resultCh := make(chan startChildResult, 1)
+	msg := startChildMsg{
 		node:       nodeFn,
 		resultChan: resultCh,
-	}
-
-	// block until the supervisor can handle the request, in case the
-	// supervisor is stopped, this line is going to panic
-	msg := ctrlMsg{
-		tag: startChild,
-		msg: startmsg,
 	}
 
 	select {
@@ -234,19 +261,10 @@ func (dyn *DynSupervisor) Spawn(nodeFn Node) (func() error, error) {
 			panic("could not get the result of a spawn call. Implementation error")
 		}
 
-		switch v := result.(type) {
-		// successful case
-		case string:
-			return dyn.terminateNode(v), nil
-
-			// error case
-		case error:
-			return nil, v
-
-			// unknown case
-		default:
-			panic("did not get valid response value from control message. Implementation error")
+		if result.startErr != nil {
+			return nil, result.startErr
 		}
+		return dyn.terminateNode(result.childName), nil
 	case <-time.After(1 * time.Second):
 		// Paranoid timeout. Better to not hang if this ever happens; to be honest,
 		// not sure when this is the case :shrug:
