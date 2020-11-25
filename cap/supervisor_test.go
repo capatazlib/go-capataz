3fb61dc0856fb6cfae07c5dfc4ac40ab744791b0
Roman Gonzalez <open-source@roman-gonzalez.info>
2020-11-25 11:31:25 -0800

    Fix bug on supervision tree termination
    Problem:
    
    When a child was returning an error on termination, the API was throwing a panic
    about an invalid SupervisorError record being created.
    
    This was an implementation error that needed to be addressed.
    
    Solution:
    
    * We created a test that replicated the issue at hand, and improved the
    implementation of the Error method from the SupervisorError type.
    
    * We also improved upon the reported message, building sections on the error
    message.

--- cap/supervisor_test.go
+++ cap/supervisor_test.go
@@ -7,6 +7,7 @@ package cap_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -299,4 +300,33 @@ func TestDoubleTermination(t *testing.T) {
 			SupervisorTerminated("root"),
 		},
 	)
+}
+
+func TestFailedTerminationOnOneLevelTree(t *testing.T) {
+	events, err := ObserveSupervisor(
+		context.TODO(),
+		"root",
+		cap.WithNodes(
+			WaitDoneWorker("child0"),
+			FailTerminationWorker("child1", fmt.Errorf("child1 failed")),
+			WaitDoneWorker("child2"),
+		),
+		[]cap.Opt{},
+		func(EventManager) {},
+	)
+
+	assert.Error(t, err)
+	t.Run("starts and stops routines in the correct order", func(t *testing.T) {
+		AssertExactMatch(t, events,
+			[]EventP{
+				WorkerStarted("root/child0"),
+				WorkerStarted("root/child1"),
+				WorkerStarted("root/child2"),
+				SupervisorStarted("root"),
+				WorkerTerminated("root/child2"),
+				WorkerFailedWith("root/child1", "child1 failed"),
+				WorkerTerminated("root/child0"),
+				SupervisorFailed("root"),
+			})
+	})
 }
